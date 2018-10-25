245210090e6065184e4834eefd8dc122c776095d
Mercy1998 <1143776778@qq.com>
2018-10-25 09:59:41 +0800

    post

--- js/register.js
+++ js/register.js
@@ -7,14 +7,11 @@
  **/
 
 $(document).ready(function () {
-    /*$('#birthYear').datetimepicker({
-        startView: 'decade',
-        minView: 'decade',
-        format: 'yyyy',
-        weekStart: 1,
-        autoclose: true,
-        todayBtn: true
-    });*/
+
+    var startHeight = 150;
+    var endHeight = 200;
+    var startYear = 1950;
+    var endYear = new Date().getFullYear();
 
 
     $('#registerBtn').click(function () {
@@ -23,11 +20,7 @@ $(document).ready(function () {
         var sexChecked=$('input:radio[name="sex"]:checked').val();
         if($("#inputName").val()==""||$('#inputHeight').val()==''||$('#yearBirth').val()==''
             ||sexChecked==null){
-            $("#alertMsg").text("信息未填写完整！").show();
-            $('#alertMsg').css({
-                'font-weight':"bold",
-                'font-size':'20px'
-            });
+            $("#alertMsg").text("信息未填写完整").show();
             if($("#inputName").val()==""){
                 $('#inputName').css('border','1px red solid');
             }
@@ -43,20 +36,13 @@ $(document).ready(function () {
 
             return;
         }
-
-
-            //性别
-
-
-            //出生年份
-
-
-        //身高
-
-        //校验通过后，向后端发送post请求 $.post
-        
+        //若全部填写
 
        else{
+            // console.log( $('#inputName').val());
+            // console.log( $(sexChecked));
+            // console.log( $('#inputHeight').val());
+            // console.log( $('#yearBirth').val());
             $('#inputName').css('border','none');
             $('.sexLabel').css('color','#ffffff');
             $('#inputHeight').css('border','none');
@@ -64,20 +50,42 @@ $(document).ready(function () {
             $('#alertMsg').text("请到魔镜设备上录入您的人脸信息").show();
             $('#registerBtn').css({
                 'background-color': '#E9E9E9',
-                'color': '#C7C4C4',
-                'font-size':"17px",
+                'color': '#999999',
                 'border': '1px #E9E9E9 solid'
             });
             $('#registerBtn').text('注册中...');
 
-            $.post()
-        }
+            $.post("http://localhost:5000/register",
+                {
+                    name:$('#inputName').val(),
+                    gender:sexChecked,
+                    height:$('#inputHeight').val(),
+                    year:$('#yearBirth').val()
+            },data=> {
+                //返回成功
+                if(data.status==100){
+
+                    $('#alertMsg').text("注册成功！").show();
+                    $('#registerBtn').css({
+                        'background-color': 'transparent',
+                        'color': '#ffffff',
+                        'border': '1px #E9E9E9 solid'
+                    });
+                    $('#registerBtn').text('注册');
 
 
-    });
+                }
+
+
+
+                    });
+
+            }
+        });
+
+
     //出生年份
-    var startYear = 1950;
-    var endYear = new Date().getFullYear();
+
     for (let i = startYear; i <= endYear; i++) {
        // console.log(i);
         $('#yearBirth').append("<option value='" + i + "'>" + i + " 年</option>");
@@ -87,14 +95,13 @@ $(document).ready(function () {
 
     });
     //身高
-    var startHeight = 150;
-    var endHeight = 200;
+
     for (let j = startHeight; j <= endHeight; j++) {
        // console.log(j);
         $('#inputHeight').append("<option value='" + j + "'>" + j + " cm</option>");
     };
     $('#inputHeight').click(function () {
-        $(this).css({ 'color': '#000000' });
+        $(this).css({ 'color': '#000000' })
 
     });
 })
