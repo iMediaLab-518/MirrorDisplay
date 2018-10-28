95734e6de5787c55341e6aea6feafd229193b5b9
Mercy1998 <1143776778@qq.com>
2018-10-28 19:44:51 +0800

    msg

--- js/register.js
+++ js/register.js
@@ -17,81 +17,101 @@ $(document).ready(function () {
     $('#registerBtn').click(function () {
         //step1:校验各字段，只做判空处理
         //姓名、身高、生日、性别选择
-        var sexChecked = $('input:radio[name="sex"]:checked').val();
-        if ($("#inputName").val() == "" || $('#inputHeight').val() == '' || $('#yearBirth').val() == ''
-            || sexChecked == null) {
-            $("#alertMsg").text("信息未填写完整").show();
-            if ($("#inputName").val() == "") {
-                $('#inputName').css('border', '1px red solid');
-            }
-            if (sexChecked == null) {
-                $('.sexLabel').css('color', 'red');
-            }
-            if ($('#inputHeight').val() == '') {
-                $('#inputHeight').css('border', '1px red solid');
-            }
-            if ($('#yearBirth').val() == '') {
-                $('#yearBirth').css('border', '1px red solid');
-            }
-
-            return;
-        }
-        //若全部填写
-
-        else {
-            console.log($('#inputName').val());
-            console.log(sexChecked);
-            console.log($('#inputHeight').val());
-            console.log($('#yearBirth').val());
-            $('#inputName').css('border', 'none');
-            $('.sexLabel').css('color', '#ffffff');
-            $('#inputHeight').css('border', 'none');
-            $('#yearBirth').css('border', 'none');
-            $('#alertMsg').text("请到魔镜设备上录入您的人脸信息").show();
-            $('#registerBtn').css({
-                'background-color': '#E9E9E9',
-                'color': '#999999',
-                'border': '1px #E9E9E9 solid'
-            });
-            $('#registerBtn').attr('disabled', 'disabled');
-            $('#registerBtn').text('注册中...');
-
-            $.post("http://192.168.2.244:5000/register",
-                {
-                    name: $('#inputName').val(),
-                    gender: sexChecked,
-                    year: $('#yearBirth').val(),
-                    height: $('#inputHeight').val()
-
-                }, data => {
-                    //返回成功
-                    if (data.status == 100) {
-
-                        $('#alertMsg').text("注册成功！").show();
-                        $('#registerBtn').css({
-                            'background-color': 'transparent',
-                            'color': '#ffffff',
-                            'border': '1px #E9E9E9 solid'
-                        });
-                        $('#registerBtn').text('注册');
-
-
+        $.get('http://localhost:5000/message',data=>{
+            if(data.status==100){
+                if(data.out==null){       //若用户未在登录，执行注册
+                    $.post('http://localhost:5000/message',
+                        {
+                        msg:'register'
                     }
-                    else {
-                        $('#alertMsg').text("注册失败!请重试!").show();
-                        $('#registerBtn').css({
-                            'background-color': 'transparent',
-                            'color': '#ffffff',
-                            'border': '1px #E9E9E9 solid'
+                    ,data=>{
+                        if(data.status==100){
+                            var sexChecked = $('input:radio[name="sex"]:checked').val();
+                            if ($("#inputName").val() == "" || $('#inputHeight').val() == '' || $('#yearBirth').val() == ''
+                                || sexChecked == null) {
+                                $("#alertMsg").text("信息未填写完整").show();
+                                if ($("#inputName").val() == "") {
+                                    $('#inputName').css('border', '1px red solid');
+                                }
+                                if (sexChecked == null) {
+                                    $('.sexLabel').css('color', 'red');
+                                }
+                                if ($('#inputHeight').val() == '') {
+                                    $('#inputHeight').css('border', '1px red solid');
+                                }
+                                if ($('#yearBirth').val() == '') {
+                                    $('#yearBirth').css('border', '1px red solid');
+                                }
+
+                                return;
+                            }
+                            //若全部填写
+
+                            else {
+                                console.log($('#inputName').val());
+                                console.log(sexChecked);
+                                console.log($('#inputHeight').val());
+                                console.log($('#yearBirth').val());
+                                $('#inputName').css('border', 'none');
+                                $('.sexLabel').css('color', '#ffffff');
+                                $('#inputHeight').css('border', 'none');
+                                $('#yearBirth').css('border', 'none');
+                                $('#alertMsg').text("请到魔镜设备上录入您的人脸信息").show();
+                                $('#registerBtn').css({
+                                    'background-color': '#E9E9E9',
+                                    'color': '#999999',
+                                    'border': '1px #E9E9E9 solid'
+                                });
+                                $('#registerBtn').attr('disabled', 'disabled');
+                                $('#registerBtn').text('注册中...');
+
+                                $.post("http://192.168.2.244:5000/register",
+                                    {
+                                        name: $('#inputName').val(),
+                                        gender: sexChecked,
+                                        year: $('#yearBirth').val(),
+                                        height: $('#inputHeight').val()
+
+                                    }, data => {
+                                        //返回成功
+                                        if (data.status == 100) {
+
+                                            $('#alertMsg').text("注册成功！").show();
+                                            $('#registerBtn').css({
+                                                'background-color': 'transparent',
+                                                'color': '#ffffff',
+                                                'border': '1px #E9E9E9 solid'
+                                            });
+                                            $('#registerBtn').text('注册');
+
+
+                                        }
+                                        else {
+                                            $('#alertMsg').text("注册失败!请重试!").show();
+                                            $('#registerBtn').css({
+                                                'background-color': 'transparent',
+                                                'color': '#ffffff',
+                                                'border': '1px #E9E9E9 solid'
+                                            });
+                                            $('#registerBtn').text('注册');
+                                        }
+
+
+
+                                    });
+
+                            }
+                        }
                         });
-                        $('#registerBtn').text('注册');
-                    }
+                }
+                else{
+                    setTimeout(function () {
+                        $('#registerBtn').click();
+                    },2000);
+                }
+            }
+        });
 
-
-
-                });
-
-        }
     });
 
 
